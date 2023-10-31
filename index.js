e74a97acec34d23befd2586f3d6747acaed64686
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2023-10-31 19:14:27 +0100

    final design 1.0

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-document.querySelector(".rocket").addEventListener("click", function () {
-  var rocket = document.querySelector(".rocket");
-  rocket.classList.add("rocket-move");
-
-  setTimeout(function () {
-    rocket.classList.remove("rocket-move");
-  }, 100);
-});
+document.getElementById("myImage").setAttribute("draggable", false);
