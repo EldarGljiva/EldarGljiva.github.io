10f6a3e97cf9c564694039874bb1e52bde38d1f5
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2023-11-01 10:07:11 +0100

    final updates 1.0

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+//Code that makes profile image not draggable
 document.getElementById("myImage").setAttribute("draggable", false);
+//Code that sends an email once the button is pressed
+function sendEmail() {
+  window.location = "mailto:eldargljiva.eg@gmail.com";
+}
